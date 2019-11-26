b4e3e381efe585a6d617501b78109235b39752bd
vladislava-st <56928640+vladislava-st@users.noreply.github.com>
2019-11-26 23:09:06 +0200

    Delete IAuditingPlanningProjectBusinessWorkflowService.java

--- msd.t2_c4.spring_domain/src/main/java/org/scrum/domain/services/IAuditingPlanningProjectBusinessWorkflowService.java
+++
@@ -1,8 +0,0 @@
-package org.scrum.domain.services;
-
-import org.scrum.domain.project.Feature;
-import org.scrum.domain.project.Project;
-
-public interface IAuditingPlanningProjectBusinessWorkflowService {
-	void auditProjectFeature(Integer projectId, String featureName);
-}
