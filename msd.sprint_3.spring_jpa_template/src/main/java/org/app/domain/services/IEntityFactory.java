8ade790f04d99b7e49bcfa7416ad0864e5f4fab7
Oana <Oana@192.168.0.18>
2019-12-07 14:42:33 -0900

    update factories

--- msd.sprint_3.spring_jpa_template/src/main/java/org/app/domain/services/IEntityFactory.java
+++ msd.sprint_3.spring_jpa_template/src/main/java/org/app/domain/services/IEntityFactory.java
@@ -6,18 +6,30 @@ import org.app.domain.RootEntity;
 
 public interface IEntityFactory {
 
-	///creare feedback cu atasamente
-		public Feedback feedbackAtasamente (Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
-				String titlu, String descriere, String tipFeedback, List<Atasament> listaAtasamente);
+	///creare feedback cu X atasamente
+		public Feedback feedbackXAtasamente (Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
+				String titlu, String descriere, String tipFeedback, List<Integer> IduriAtasamente);
 	
-	///creare feedback cu comentarii
-		public Feedback feedbackComentarii k(Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
-				String titlu, String descriere, String tipFeedback, List<Comentariu> listaComentarii);
+	///creare feedback cu X comentarii
+		public Feedback feedbackXComentarii k(Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
+				String titlu, String descriere, String tipFeedback, List<Integer> IduriComentarii);
 			
-	///creare feedback cu cerinte
-		public Feedback feedbackCerinte (Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
-				String titlu, String descriere, String tipFeedback, List<Cerinta> listaCerinte);
+	///creare feedback cu X cerinte
+		public Feedback feedbackXCerinte (Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
+				String titlu, String descriere, String tipFeedback, List<Integer> IduriCerinte);
 		
-	public void initDomainServiceEntities() ;
+	///creare feedback cu atasament
+		public Feedback feedback1Atasament (Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
+			String titlu, String descriere, String tipFeedback);
+		
+	///creare feedback cu comentariu
+		public Feedback feedback1Comentariu (Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
+			String titlu, String descriere, String tipFeedback);
+		
+	///creare feedback cu cerinta
+		public Feedback feedback1eCerinta (Integer idFeedback, Status status, Proiect proiect, User user, Date dataInregistrare, Date deadline,
+				String titlu, String descriere, String tipFeedback);
+		
+		public void initDomainServiceEntities() ;
 	
 }
