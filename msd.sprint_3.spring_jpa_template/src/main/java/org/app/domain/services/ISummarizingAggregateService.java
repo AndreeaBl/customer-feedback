8b0b86cb1ecff8e0208873eaa8a7726675fe59ba
Oana <Oana@192.168.0.18>
2019-12-08 09:42:14 -0900

    SummarizingService

--- msd.sprint_3.spring_jpa_template/src/main/java/org/app/domain/services/ISummarizingAggregateService.java
+++ msd.sprint_3.spring_jpa_template/src/main/java/org/app/domain/services/ISummarizingAggregateService.java
@@ -1,9 +1,17 @@
 package org.app.domain.services;
 
+import org.app.domain.Feedback;
 import org.app.domain.RootEntity;
+
 
 public interface ISummarizingAggregateService {
 
 	RootEntity countingComponents(RootEntity project);
+	
+	Feedback countingComentarii(Feedback feedback);
+	
+	Feedback countingAtasamente(Feedback feedback);
+	
+	Feedback countingCerinte(Feedback feedback);
 	
 }
--- msd.sprint_3.spring_jpa_template/src/main/java/org/app/domain/services/SummarizingAggregateServiceImpl.java
+++ msd.sprint_3.spring_jpa_template/src/main/java/org/app/domain/services/SummarizingAggregateServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.logging.Logger;
 
 import org.app.domain.RootEntity;
 import org.springframework.stereotype.Service;
+import org.app.domain.Feedback;
+
 
 /*
  * Naming template: Action.ing Entity-name Service-type
@@ -19,4 +21,30 @@ public class SummarizingAggregateServiceImpl implements ISummarizingAggregateSer
 		rootEntity.setComponentsCount(componentsCount);
 		return rootEntity;
 	}
+	
+	@Override
+	public Feedback countingComentarii(Feedback feedback) {
+		logger.info(">>> Service: SummarizingAggregateServiceImpl :: countingComentarii.");
+		Integer comentariuCount = (feedback.getListaComentarii() == null) ? 0 : feedback.getListaComentarii().size();
+		feedback.setComentariuCount(comentariuCount);
+		return feedback;
+	}
+	
+	@Override
+	public Feedback countingAtasamente(Feedback feedback) {
+		logger.info(">>> Service: SummarizingAggregateServiceImpl :: countingAtasamente.");
+		Integer atasamentCount = (feedback.getListaAtasamente() == null) ? 0 : feedback.getListaAtasamente().size();
+		feedback.setAtasamentCount(atasamentCount);
+		return feedback;
+	}
+	
+	@Override
+	public Feedback countingCerinte(Feedback feedback) {
+		logger.info(">>> Service: SummarizingAggregateServiceImpl :: countingCerinte.");
+		Integer cerintaCount = (feedback.getListaCerinte() == null) ? 0 : feedback.getListaCerinte().size();
+		feedback.setCerintaCount(cerintaCount);
+		return feedback;
+	} 
+	
+	
 }
